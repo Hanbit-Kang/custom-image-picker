3da07b4cab5a1b842e20d8046a155c5090abb618
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-23 13:46:21 +0900

    Implement ImageAdapter

--- app/src/main/java/com/example/customimagepicker/HomeFragment.kt
+++ app/src/main/java/com/example/customimagepicker/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.customimagepicker.adapter.ImageAdapter
 import com.example.customimagepicker.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
--- app/src/main/java/com/example/customimagepicker/adapter/ImageAdapter.kt
+++ app/src/main/java/com/example/customimagepicker/adapter/ImageAdapter.kt
@@ -1,4 +1,64 @@
 package com.example.customimagepicker.adapter
 
-class ImageAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.customimagepicker.R
+import com.example.customimagepicker.data.ImageItem
+import com.example.customimagepicker.databinding.ItemImageBinding
+import com.example.customimagepicker.viewmodel.ImagePickerViewModel
+
+class ImageAdapter(
+    private val parentViewModel: ImagePickerViewModel
+): ListAdapter<ImageItem, RecyclerView.ViewHolder>(ImageDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = DataBindingUtil.inflate<ItemImageBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_image,
+            parent,
+            false
+        )
+
+        val holder = ImageViewHolder(binding)
+        subscribeUi(binding, holder)
+
+        return holder
+    }
+
+    private fun subscribeUi(binding: ItemImageBinding, holder: ImageViewHolder) {
+        binding.checkbox.setOnCheckedChangeListener { _, isChecked ->
+            if(parentViewModel.imageItemList.value == null) return@setOnCheckedChangeListener
+
+            val position = holder.absoluteAdapterPosition
+            parentViewModel.imageItemList.value!![position].isChecked = isChecked
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val imageItem = getItem(position)
+        (holder as ImageViewHolder).bind(imageItem)
+    }
+
+    class ImageViewHolder(
+        private val binding: ItemImageBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(imageItem: ImageItem) {
+            Glide.with(binding.root)
+                .load(imageItem.uri)
+                .into(binding.image)
+        }
+    }
+}
+private class ImageDiffCallback: DiffUtil.ItemCallback<ImageItem>() {
+    override fun areItemsTheSame(oldItem: ImageItem, newItem: ImageItem): Boolean {
+        return oldItem.uri == newItem.uri
+    }
+
+    override fun areContentsTheSame(oldItem: ImageItem, newItem: ImageItem): Boolean {
+        return oldItem == newItem
+    }
 }
