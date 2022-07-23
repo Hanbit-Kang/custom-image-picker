3fd815efaa392c3bf3bae72a94bb4eb05b723488
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-23 13:58:31 +0900

    Implement ImagePickerFragment, passing data to HomeFragment

--- app/src/main/java/com/example/customimagepicker/HomeFragment.kt
+++ app/src/main/java/com/example/customimagepicker/HomeFragment.kt
@@ -1,12 +1,12 @@
 package com.example.customimagepicker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.customimagepicker.adapter.ImageAdapter
 import com.example.customimagepicker.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -23,6 +23,25 @@ class HomeFragment : Fragment() {
             findNavController().navigate(directions)
         }
 
+        activity?.supportFragmentManager?.setFragmentResultListener(URI_LIST_CHECKED, viewLifecycleOwner) { _, bundle ->
+            bundle.getStringArrayList("uriList")?.let { uriList ->
+                for(uri in uriList) {
+                    Log.d(TAG, uri.toString())
+                    // TODO
+                }
+            }
+        }
+
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    companion object {
+        const val URI_LIST_CHECKED = "URI_LIST_CHECKED"
+        const val TAG = "HomeFragment"
     }
 }
--- app/src/main/java/com/example/customimagepicker/ImagePickerFragment.kt
+++ app/src/main/java/com/example/customimagepicker/ImagePickerFragment.kt
@@ -5,13 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.customimagepicker.HomeFragment.Companion.URI_LIST_CHECKED
+import com.example.customimagepicker.adapter.ImageAdapter
+import com.example.customimagepicker.databinding.FragmentImagePickerBinding
+import com.example.customimagepicker.viewmodel.ImagePickerViewModel
 
 class ImagePickerFragment : Fragment() {
+    private var _binding: FragmentImagePickerBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: ImagePickerViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_picker, container, false)
+        _binding = FragmentImagePickerBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.buttonListener = View.OnClickListener {
+            activity?.supportFragmentManager?.setFragmentResult(
+                URI_LIST_CHECKED,
+                bundleOf("uriList" to viewModel.getCheckedImageUriList())
+            )
+            findNavController().navigateUp()
+        }
+
+        val adapter = ImageAdapter(viewModel)
+        binding.recyclerviewImage.adapter = adapter
+
+        viewModel.fetchImageItemList(requireContext())
+        subscribeUi(adapter)
+
+        return binding.root
+    }
+
+    private fun subscribeUi(adapter: ImageAdapter) {
+        viewModel.imageItemList.observe(viewLifecycleOwner) { imageItemList ->
+            adapter.submitList(imageItemList)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
