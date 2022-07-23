58661fc3c9167d5f5435ac8689a2b43df426e40b
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-23 13:06:37 +0900

    Add HomeFragment, ImagePickerFragment, nav_graph, Implement navigation

---
+++ app/src/main/java/com/example/customimagepicker/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.customimagepicker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.customimagepicker.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.buttonListener = View.OnClickListener {
+            val directions = HomeFragmentDirections.actionHomeToImagepicker()
+            findNavController().navigate(directions)
+        }
+
+        return binding.root
+    }
+}
